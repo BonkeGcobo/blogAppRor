a480ad007ec8ae772133b612b6219afed556e4e6
bonkegcobo <madixmicro@gmail.com>
2023-10-27 22:37:30 +0200

    fix linter errors0

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  
+
   after_save :update_likes
-  
+
   private
+
   def update_likes
     post.increment!(:likes_counter)
   end
