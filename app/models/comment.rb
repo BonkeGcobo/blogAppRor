bc5bec0fbc278e62158f70363cd9cef453e65c95
bonkegcobo <madixmicro@gmail.com>
2023-10-27 22:36:49 +0200

    add rails active callbacks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
-
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  
+  after_save :update_number_of_comments
+  
+  private
   def update_number_of_comments
     post.increment!(:comments_counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
+  
+  after_save :update_counter
+
+  private
   def update_counter
-    user.increment!(:post_counter)
+    author.increment!(:post_counter)
   end
 
   def recent_comments
