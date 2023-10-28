9688be469ca755539fa75d4faebdcdd2ffa0434f
bonkegcobo <madixmicro@gmail.com>
2023-10-28 10:47:41 +0200

    name not blank validation added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
-  
+
 
   def recent_post
     posts.order(created_at: :desc).limit(3)
