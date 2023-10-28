1c7751bcbfc38526c522e8d3ed049b6ed804181c
bonkegcobo <madixmicro@gmail.com>
2023-10-28 19:19:19 +0200

    add all necessary test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,6 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  describe 'Test for validation of likes' do
+    author_user_post = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, author: author_user_post)
+    
+    subject{Like.new(post:post, author:author_user_post)}
+    before {subject.save}
+
+    it 'Post should be not be blank' do
+        subject.post = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Author of like should not be blank' do
+        subject.author = nil
+        expect(subject).to_not be_valid
+    end
+
+  end
+
+
   describe 'private method update_likes' do
     it 'increments the likes_counter of the associated post' do
       user = FactoryBot.create(:user)
