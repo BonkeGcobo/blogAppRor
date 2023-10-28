aaa837c6ee48ac9e29721ea0eefc93be35f53dbe
bonkegcobo <madixmicro@gmail.com>
2023-10-28 10:46:57 +0200

    name presence test written for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    #tests go here
+    subject{User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+    
+    before{subject.save}
+
+    it 'Name should be not blank' do
+        subject.name = nil
+        expect(subject).to_not be_valid
+    end
+end
